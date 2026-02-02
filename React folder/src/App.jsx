bb16612b734e225d796a44e564e3889da4dd927d
Zohaib-617 <zk1209815@gmail.com>
2026-02-02 23:41:23 +0500

    React folder done

---
+++ React folder/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+const App = () => {
+  return (
+    <>
+    <div id="dad">
+    <h1>hello</h1>
+    <h2>hello2</h2>
+    </div>
+    <div id="chacha"></div>
+    
+    </>
+  )
+}
+
+export default App
